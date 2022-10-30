de78f6ae82c5357f99dcb7e2f1f94584b9892acf
elie sultan <neguedayinhara@gmail.com>
2022-10-30 20:42:21 +0200

    adklsfjdkls

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-var mysql      = require('mysql');
-
+var mysql = require('mysql');
+const axios = require("axios");
 
 
 
@@ -33,21 +33,37 @@ app.get('/sql', function (req, res) {
 })
 
 app.get('/insert', async (req, res) => {
-    console.log('req.body', req.query )
-    let requete1 = req.query.request
-    let result1 = req.query.resultat
+    //console.log('req.body', req.query )
+    // http://localhost:3030/insert?request=test&resultat=essai
 
-    let values = [requete1, result1]
-    console.log("values", values)
-    let sql = "INSERT INTO result (request, result) VALUES (?,?)"
-    connection.query(sql, values, function (err, result) {
-        if (err) throw err;
-        console.log("Number of records inserted: " + result.affectedRows);
-    });
-    connection.end();
+    let array = ['create'];
+
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        console.log('element', element)
+        let postAxios =  await axios.post(`https://lionfish-app-xheyf.ondigitalocean.app/${element}`)
+        console.log('postAxios',postAxios.data)
+        if (postAxios.data !== { api: 'GRAPHQL', version: '1.0' }) {
+            console.log('hacking possible')
+        }
+    }
+
+    // console.log('alphabet', alphabet);
+        
+    // let postAxios =  await axios.get(`https://lionfish-app-xheyf.ondigitalocean.app`)
+
+    // console.log('postAxios',postAxios.data)
+
+
+    // let values = ["test4", "test4"]
+    // let sql = "INSERT INTO result (request, result) VALUES (?,?)"
+    // connection.query(sql, values, function (err, result) {
+    //     if (err) throw err;
+    //     console.log("Number of records inserted: " + result.affectedRows);
+    // });
+    // connection.end();
 
     res.send('finish')
-   // 
 })
 
 

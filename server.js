f861b9738165fbb454a3178af98b29bca1a1ed56
elie sultan <neguedayinhara@gmail.com>
2022-10-30 19:41:20 +0200

    fin cession

--- server.js
+++ server.js
@@ -1,81 +1,58 @@
-let express = require('express')
-let app = express()
-let bodyParser = require('body-parser') // permet de parser les request
-let session = require('express-session')
+const express = require('express')
+const app = express()
+var mysql      = require('mysql');
 
-// Moteur de tempates
-app.set('view engine', 'ejs')
 
-// middleware
-app.use('/assets' ,express.static('public'))
-app.use(bodyParser.urlencoded({ extended: false}))
-app.use(bodyParser.json())
-app.use(session({
-    secret: 'adafdadf', // mise en place d'une clés secrète qui servira à chiffrer notre cookie
-    resave:false,
-    saveUninitialized: true, 
-    cookie:  {secure: false} // car on ne traite pas avec de l'https
-}))
-app.use(require('./middlewares/flash'))
 
-// Routes
-app.get('/', (request, response) => {
-        let Message = require('./models/message')
-        Message.all(function (messages) {
-       response.render('pages/index', {messages: messages} ) 
+
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : '',
+  database : 'nodegraph'
+});
+ 
+connection.connect();
+ 
+connection.query('SELECT * FROM result', (err, rows) => {
+    if (err) throw err
+    console.log(rows)
+})
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/sql', function (req, res) {
+    connection.query('SELECT * FROM result', (err, rows) => {
+        if (err) throw err
+        console.log(rows)
     })
 })
 
-app.get('/qsn', (request, response) => {
-        
-       response.render('pages/qsn' ) 
-    
-})
+app.get('/insert', async (req, res) => {
+    console.log('req.body', req.query )
+    let requete1 = req.query.request
+    let result1 = req.query.resultat
 
-app.get('/contacteznous', (request, response) => {
-        
-       response.render('pages/contacteznous' ) 
-    
-})
+    let values = [requete1, result1]
+    console.log("values", values)
+    let sql = "INSERT INTO result (request, result) VALUES (?,?)"
+    connection.query(sql, values, function (err, result) {
+        if (err) throw err;
+        console.log("Number of records inserted: " + result.affectedRows);
+    });
+    connection.end();
 
-app.get('/mag', (request, response) => {
-        
-       response.render('pages/mag' ) 
-    
-})
-
-
-
-// que se passe t'il quand j'ai une une requete qui est envoyé
-app.post('/', (request, response) => {
-
-    // si elle est fausse, je la rentre dans l'erreur
-        if ( request.body.message === undefined || request.body.message === '') 
-            {
-               
-                request.flash('error', "vous n'avez pas posté de message")
-                
-                
-                response.redirect('/')
-                // sinon, je la stock dans un base de données
-            } else {
-
-                let Message = require('./models/message')
-                Message.create(request.body.message, function () { 
-                    request.flash('success', "Merci !")
-                    response.redirect('/')
-                })
-            }
-        
-    })
-
-app.get('/message/:id', (request, response) => {
-    let Message = require('./models/message')
-    Message.find(request.params.id, function (message) {
-        response.render('messages/show', {message: message})
-    })
+    res.send('finish')
+   // 
 })
 
 
-app.listen('livreor770.herokuapp.com')
 
+
+
+
+app.listen(3030)
